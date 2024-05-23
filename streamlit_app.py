679aabba550dcec6d235a4c2bb0509d562ac86e4
Eduardo Martinez Valdez <111398198+EduardoMtzValdez@users.noreply.github.com>
2024-05-23 11:37:36 -0600

    Update streamlit_app.py
    v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,25 @@
-import streamlit
-import snowflake.connector
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
 
-streamlit.title('My Parents New Healthy Dinner')
+st.title('My Parents New Healthy Dinner')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-CURRENT_REGION()")
+# Write directly to the app
+#st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+#st.write(    """**Choose the fruits you want in your custom Smoothie!**""")
+
+#name_on_order = st.text_input('Name on Smoothie:')
+#st.write('The name on your Smoothie will be:', name_on_order)
+
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+my_cur = session.cursor()
+session.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = session.fetchone()
+
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
