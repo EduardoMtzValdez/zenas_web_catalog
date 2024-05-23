bd419580f0da5f1f2c415ad7b9b68f8e4c3ca726
Eduardo Martinez Valdez <111398198+EduardoMtzValdez@users.noreply.github.com>
2024-05-23 13:01:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,6 @@ price = df2.collect()[0][1]
 # Mostrando la Imagen
 st.image(url, width=400, caption= product_caption)
 
-st.text("Price: ", price)
-#st.text("Sizes Available: ", df2.collect()[0][2])
-#st.text("Description: ", df2.collect()[0][3])
+st.text("Price: " + price)
+st.text("Sizes Available: " + df2.collect()[0][2])
+st.text("Description: " + df2.collect()[0][3])
