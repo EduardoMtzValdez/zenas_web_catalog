8b0f016cd8f8a5f66604b5b51bae9c12102496ef
Eduardo Martinez Valdez <111398198+EduardoMtzValdez@users.noreply.github.com>
2024-05-23 12:27:40 -0600

    Update streamlit_app.py
    v13

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,20 @@ df = my_dataframe.to_pandas()
 # Let's put a pick list here so they can pick the color
 option = st.selectbox('Pick a sweatsuit color or style:', color_list)
 
-st.stop()
+#st.stop()
 
 ## We'll build the image caption now, since we can
-#product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 ## use the option selected to go back and get all the info from the database
+df2 = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE").select(col('DIRECT_URL'), col('PRICE'), col('SIZE_LIST'), col('UPSELL_PRODUCT_DESC')).where("color_or_style = '" + option + "'")
 #my_cur.execute("select direct_url, price, size_list, upsell_product_desc from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website where color_or_style = '" + option + "';")
 #df2 = my_cur.fetchone()
-#streamlit.image(
-#df2[0],
-#width=400,
-#caption= product_caption
-#)
-#streamlit.write('Price: ', df2[1])
-#streamlit.write('Sizes Available: ',df2[2])
-#streamlit.write(df2[3])
+streamlit.image(
+df2[0],
+width=400,
+caption= product_caption
+)
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ',df2[2])
+st.write(df2[3])
+
