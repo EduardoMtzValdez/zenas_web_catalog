3c156b12ccc11b41d298dcc9e69115edfec75ca8
Eduardo Martinez Valdez <111398198+EduardoMtzValdez@users.noreply.github.com>
2024-05-23 12:14:24 -0600

    Update streamlit_app.py
    v9

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,12 @@ my_dataframe = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")
 df = my_dataframe.to_pandas()
 
 # temp write the dataframe to the page so I Can see what I am working with
-streamlit.write(df)
+st.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist()
 print(color_list)
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = st.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
 st.stop()
 
